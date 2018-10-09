0e8dcb98af49e7f5fc8cb3edc961748f9015b3c7
Sony <lo.license@yandex.ru>
2018-10-09 20:51:36 +0300

    Создание всех роутов

---
+++ controllers/position.js
@@ -0,0 +1,13 @@
+module.exports.getByCategoryId = (req, res)=>{
+    
+}
+module.exports.create = (req, res)=>{
+    
+}
+
+module.exports.remove = (req, res)=>{
+    
+}
+module.exports.update = (req, res)=>{
+    
+}
