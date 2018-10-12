89c7b9c70045d5a57049af65bea1fd307ca3d239
Sony <lo.license@yandex.ru>
2018-10-12 21:27:46 +0300

    Final auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 const keys = require('../config/keys')
+const errorHandler = require('../utils/errorHandler')
 
 
 module.exports.login = async function(req, res) {
@@ -59,6 +60,7 @@ module.exports.register = async function(req, res) {
       res.status(201).json(user)
     } catch(e) {
       // Обработать ошибку
+      errorHandler(res, e)
     }
 
   }
