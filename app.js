446f281e76b6e16f8b94c6cc95a14f3d6bc3db91
Sony <lo.license@yandex.ru>
2018-10-13 09:11:51 +0300

    Добавление и обновление категории

--- app.js
+++ app.js
@@ -19,9 +19,11 @@ mongoose.connect(keys.mongoURL)
     console.log(err)
 })
 
+
 app.use(passport.initialize())
 require('./middleware/passport')(passport)
 app.use(require('morgan')('dev'))
+app.use('/uploads', express.static('uploads'))
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 app.use(require('cors')())
